9ca47d32cf034e567da6387c89b006f999accc1c
Shaik Abdul Kader <58418260+shaikabdlkadr@users.noreply.github.com>
2025-02-23 22:25:25 +0530

    Upload the local files

---
+++ 1. Danny's Diner/Queries.sql
@@ -0,0 +1,17 @@
+-- Verifying the imports were done
+
+SELECT * FROM dannys_diner.sales;
+
+SELECT * FROM dannys_diner.menu;
+
+SELECT * FROM dannys_diner.members;
+
+
+-- 1.  What is the total amount each customer spent at the restaurant?
+
+SELECT s.customer_id, SUM(m.price) AS total_sales
+FROM dannys_diner.sales AS s
+JOIN dannys_diner.menu AS m
+    ON s.product_id = m.product_id
+GROUP BY s.customer_id
+ORDER BY 1 ASC;
