0a9d183a264279faeade0b51c767d064e737732f
Shaik Abdul Kader <shaikabdulkader.s00@gmail.com>
2025-02-25 13:33:53 +0530

    Added more test data into the sales, menu and members table data were get from ChatGPT for more transformation

--- 1. Danny's Diner/Schema.sql
+++ 1. Danny's Diner/Schema.sql
@@ -53,3 +53,65 @@ INSERT INTO members
 VALUES
   ('A', '2021-01-07'),
   ('B', '2021-01-09');
+
+
+-- Additional sales data  
+INSERT INTO sales
+  ("customer_id", "order_date", "product_id")
+VALUES
+  ('A', '2021-01-15', '1'),
+  ('A', '2021-01-17', '2'),
+  ('B', '2021-02-05', '1'),
+  ('B', '2021-02-10', '2'),
+  ('B', '2021-02-15', '3'),
+  ('C', '2021-01-10', '1'),
+  ('C', '2021-01-12', '2'),
+  ('C', '2021-01-15', '3'),
+  ('C', '2021-02-01', '1'),
+  ('D', '2021-01-02', '1'),
+  ('D', '2021-01-04', '3'),
+  ('D', '2021-01-06', '2'),
+  ('D', '2021-02-20', '3'),
+  ('E', '2021-01-03', '2'),
+  ('E', '2021-01-07', '3'),
+  ('E', '2021-01-08', '1');
+
+-- Additional menu items  
+INSERT INTO menu
+  ("product_id", "product_name", "price")
+VALUES
+  ('4', 'steak', '20'),
+  ('5', 'pizza', '18'),
+  ('6', 'pasta', '14');
+
+-- Additional member registrations  
+INSERT INTO members
+  ("customer_id", "join_date")
+VALUES
+  ('C', '2021-01-10'),
+  ('D', '2021-01-15'),
+  ('E', '2021-02-01');
+
+-- Additional sales data with newly added menu items  
+INSERT INTO sales
+  ("customer_id", "order_date", "product_id")
+VALUES
+  ('A', '2021-01-20', '4'), -- steak
+  ('A', '2021-01-25', '5'), -- pizza
+  ('A', '2021-01-30', '6'), -- pasta
+  ('B', '2021-02-07', '4'), -- steak
+  ('B', '2021-02-12', '6'), -- pasta
+  ('B', '2021-02-18', '5'), -- pizza
+  ('C', '2021-01-14', '4'), -- steak
+  ('C', '2021-01-20', '6'), -- pasta
+  ('C', '2021-01-25', '5'), -- pizza
+  ('C', '2021-02-03', '2'), -- curry
+  ('D', '2021-01-10', '6'), -- pasta
+  ('D', '2021-01-15', '5'), -- pizza
+  ('D', '2021-01-22', '4'), -- steak
+  ('D', '2021-02-05', '3'), -- ramen
+  ('E', '2021-01-04', '4'), -- steak
+  ('E', '2021-01-06', '5'), -- pizza
+  ('E', '2021-01-09', '6'), -- pasta
+  ('E', '2021-02-10', '1'), -- sushi
+  ('E', '2021-02-12', '2'); -- curry
